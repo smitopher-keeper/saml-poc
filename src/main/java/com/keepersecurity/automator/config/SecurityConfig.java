56a442c7404f8f0906f7ba286e76d41a69584f89
smitopher-keeper <csmith@keepersecurity.com>
2025-02-14 16:28:20 -0600

    branding

--- src/main/java/com/keepersecurity/automator/config/SecurityConfig.java
+++ src/main/java/com/keepersecurity/automator/config/SecurityConfig.java
@@ -1,29 +1,45 @@
 package com.keepersecurity.automator.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.saml2.provider.service.metadata.OpenSaml4MetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+  private final RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+
+  public SecurityConfig(RelyingPartyRegistrationRepository relyingPartyRegistrationRepository) {
+    this.relyingPartyRegistrationRepository = relyingPartyRegistrationRepository;
+  }
+
   @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-      .authorizeHttpRequests(authorize -> authorize
-          .anyRequest().authenticated()
-      )
-      // Enable SAML2 Login for SAML-based authentication.
-      .saml2Login(Customizer.withDefaults());
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    DefaultRelyingPartyRegistrationResolver relyingPartyRegistrationResolver =
+        new DefaultRelyingPartyRegistrationResolver(this.relyingPartyRegistrationRepository);
+    Saml2MetadataFilter filter =
+        new Saml2MetadataFilter(relyingPartyRegistrationResolver, new OpenSaml4MetadataResolver());
+
+    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .saml2Login(withDefaults()).saml2Logout(withDefaults())
+        .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);
     return http.build();
   }
 
   // Register the Protobuf message converter so Spring MVC handles
   // "application/x-protobuf" media type.
-  // @Bean
-  // ProtobufHttpMessageConverter protobufHttpMessageConverter() {
-  // return new ProtobufHttpMessageConverter();
-  // }
+  @Bean
+  ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+    return new ProtobufHttpMessageConverter();
+  }
 }
